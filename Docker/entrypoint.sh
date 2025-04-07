f236fad23960308a5213227ab5d451ac7a5fb335
Ivan Veremchuk <ivan.veremchuk@student.ukf.sk>
2025-04-07 19:31:22 +0200

    update user seeder and docker encrypt

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-php artisan migrate
+
+mysql -hdb -uroot -proot -e "CREATE DATABASE IF NOT EXISTS laravel;"
+
+yes | php artisan migrate --seed
 
 # Start the Laravel development server, accessible from any IP
 php artisan serve --host 0.0.0.0
